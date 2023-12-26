7ad81ab7ce783abffa15a1e8d8e25fdb308761cb
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-12-26 22:48:30 +0300

    Refactoring

--- src/main/java/com/xxavierr404/digigame/service/ChatMemberService.java
+++ src/main/java/com/xxavierr404/digigame/service/ChatMemberService.java
@@ -2,11 +2,9 @@ package com.xxavierr404.digigame.service;
 
 import com.xxavierr404.digigame.dao.ChatMemberRepository;
 import com.xxavierr404.digigame.domain.ChatMember;
-import com.xxavierr404.digigame.dto.ChatMemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,25 +12,6 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ChatMemberService {
     private final ChatMemberRepository repository;
-    private final ChatService chatService;
-    private final UserService userService;
-
-    public ChatMember create(ChatMemberDto dto) {
-        var chatMember = new ChatMember();
-        chatMember.setChat(chatService.readOne(dto.getChatId()).orElseThrow());
-        chatMember.setUser(userService.readOne(dto.getUserId()).orElseThrow());
-        chatMember.setJoinTime(LocalDateTime.now());
-        return repository.save(chatMember);
-    }
-
-    public boolean delete(Long id) {
-        var searchResult = repository.findById(id);
-        if (searchResult.isEmpty()) {
-            return false;
-        }
-        repository.deleteById(id);
-        return true;
-    }
 
     public Optional<ChatMember> findByUserIdAndChatId(Long userId, Long chatId) {
         return repository.findByUserIdAndChatId(userId, chatId);
